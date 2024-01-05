52b7fce698b569002f361ebc4d726a6eb57f369f
Cristian <125cninanya@frhsd.com>
2024-01-05 10:19:16 -0500

    inital upload

--- js/wordle.js
+++ js/wordle.js
@@ -22,8 +22,68 @@ function intialize(){
             let tile=document.createElement("span");
             tile.id=r.toString() + "-"+C.toString();
             tile.classList.add("tile");
-            tile.innerText="P";
+            tile.innerText="";
             document.getElementById("board").appendChild(tile);
         }
     }
+
+    //Listen for keyup
+    document.addEventListener("keyup", (e) => {
+        if(gameOver) return;
+
+        //alert(e.code);
+        if("KeyA" <= e.code && e.code <= "KeyZ"){
+            if (col<width){
+                let currentTile=document.getElementById(row.toString()+"-"+col.toString());
+                if(currentTile.innerText==""){
+                    currentTile.innerText=e.code[3];
+                    col++;
+                }
+                    
+            }
+        }
+            else if(e.code=="Backspace"){
+                if(0<col&&col<=width){
+                    col--;
+                }
+                let currentTile=document.getElementById(row.toString()+"-"+col.toString());
+                currentTile.innerText="";
+        }
+        else if (e.code=="Enter"){
+            update();
+            row++; //start new row
+            col=0 //start at 0 for new row 
+        }
+
+        if(!gameOver&&row==height){
+            gameOver=True;
+            document.getElementById("answer").innerText=word;
+
+        }
+    })
+}
+
+function update(){
+    let correct=0;
+    for (let c=0; c<width; c++){
+        let currentTile=document.getElementById(row.toString()+"-"+c);
+        let letter=currentTile.innerText;
+
+        //Is it in the corect position?
+        if (word[c]==letter){
+            currentTile.classList.add("correct");
+            correct++;
+        }//Is it in the word?
+        else if(word.includes(letter)){
+            currentTile.classList.add("present");
+        }//Not in the word
+        else{
+            currentTile.classList.add("absent");
+        }
+
+        if(correct==width){
+            gameOver=true;
+        }
+}
+
 }
